e4b7cd3233d4f36a13ed8f35847e3acd2e00cbdc
Avishka <dilshandell333@gmail.com>
2026-08-16 18:04:37 +0530

    Add database initialization script

---
+++ init.sql
@@ -0,0 +1,7 @@
+
+CREATE DATABASE IF NOT EXISTS `spms_user_db` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS `spms_vehicle_db` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS `spms_parking_db` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS `spms_payment_db` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+SHOW DATABASES LIKE 'spms_%';
